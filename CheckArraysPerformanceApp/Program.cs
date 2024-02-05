c8ecc6b0f379faa5977220d6adb4afe50349ee46
Anton Ivaschenko <ivaschenkoanton@yandex.ru>
2024-02-05 13:15:56 +0700

    Update to .net8

--- CheckArraysPerformanceApp/Program.cs
+++ CheckArraysPerformanceApp/Program.cs
@@ -1,77 +1,98 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 
 namespace CheckArraysPerformanceApp
 {
-	class Program
-	{
-		private const int c_numElements = 10000;
+    class Program
+    {
+        private const int ElementsCount = 30_000;
 
-		static void Main()
-		{
-			int[,] a2Dim = new int[c_numElements, c_numElements];
-			int[][] aJagged = new int[c_numElements][];
+        static void Main()
+        {
+            int[,] a2Dim = new int[ElementsCount, ElementsCount];
+            int[][] aJagged = new int[ElementsCount][];
 
-			for (int i = 0; i < c_numElements; i++)
-				aJagged[i] = new int[c_numElements];
+            for (int i = 0; i < ElementsCount; i++)
+                aJagged[i] = new int[ElementsCount];
 
 
-			CheckPerformance(() => Safe2DimArrayAccess(a2Dim));
-			CheckPerformance(() => SafeJaggedArrayAccess(aJagged));
-			CheckPerformance(() => Unsafe2DimArrayAccess(a2Dim));
+            CheckPerformance(() => Safe2DimArrayAccess(a2Dim),               "Safe matrix enumeration  ");
+            CheckPerformance(() => Unsafe2DimArrayAccess(a2Dim),             "Unsafe matrix enumeration");
+            CheckPerformance(() => ForSumForJaggedArrayAccess(aJagged), "For sum for jagged arrays    ");
+            CheckPerformance(() => ForeachSumForJaggedArray(aJagged),   "Foreach sum for jagged arrays");
+            CheckPerformance(() => LinqSumForJaggedArray(aJagged),      "LINQ sum for jagged arrays   ");
 
             Console.ReadKey();
-		}
+        }
 
-		private static void CheckPerformance(Func<int> func)
-		{
-			var sw = Stopwatch.StartNew();
-			int safe2DimArrayAccess = func();
-			TimeSpan elapsed = sw.Elapsed;
-			Console.WriteLine($"{elapsed} - {safe2DimArrayAccess}");
-		}
+        private static void CheckPerformance(Func<int> func, string methodName)
+        {
+            var sw = Stopwatch.StartNew();
+            int safe2DimArrayAccess = func();
+            TimeSpan elapsed = sw.Elapsed;
+            Console.WriteLine($"{methodName}: {elapsed} - {safe2DimArrayAccess}");
+        }
 
-		private static int Safe2DimArrayAccess(int[,] a)
-		{
-			int sum = 0;
-			for (int i = 0; i < c_numElements; i++)
-			{
-				for (int j = 0; j < c_numElements; j++)
-					sum += a[i, j];
+        private static int Safe2DimArrayAccess(int[,] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < ElementsCount; i++)
+            {
+                for (int j = 0; j < ElementsCount; j++)
+                    sum += a[i, j];
+            }
 
-			}
+            return sum;
+        }
 
-			return sum;
-		}
+        private static int ForSumForJaggedArrayAccess(int[][] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < ElementsCount; i++)
+            {
+                for (int j = 0; j < ElementsCount; j++)
+                    sum += a[i][j];
 
-		private static int SafeJaggedArrayAccess(int[][] a)
-		{
-			int sum = 0;
-			for (int i = 0; i < c_numElements; i++)
-			{
-				for (int j = 0; j < c_numElements; j++)
-					sum += a[i][j];
+            }
 
-			}
+            return sum;
+        }
 
-			return sum;
-		}
+        private static int ForeachSumForJaggedArray(int[][] a)
+        {
+            int sum = 0;
+            foreach (int[] ints in a)
+            {
+                foreach (int i in ints)
+                {
+                    sum += i;
+                }
+            }
 
-		private static unsafe int Unsafe2DimArrayAccess(int[,] a)
-		{
-			int sum = 0;
-			fixed (int* pi = a)
-			{
-				for (int i = 0; i < c_numElements; i++)
-				{
-					int baseOfDim = i * c_numElements;
-					for (int j = 0; j < c_numElements; j++)
-						sum += pi[baseOfDim + j];
-				}
-			}
+            return sum;
+        }
 
-			return sum;
-		}
-	}
+        private static int LinqSumForJaggedArray(int[][] a)
+        {
+            return a.Sum(i => i.Sum());
+        }
+
+        private static unsafe int Unsafe2DimArrayAccess(int[,] a)
+        {
+            int sum = 0;
+            fixed (int* pi = a)
+            {
+                for (int i = 0; i < ElementsCount; i++)
+                {
+                    int baseOfDim = i * ElementsCount;
+                    for (int j = 0; j < ElementsCount; j++)
+                        sum += pi[baseOfDim + j];
+                }
+            }
+
+            return sum;
+        }
+    }
 }
--- IteratorsApp/Program.cs
+++ IteratorsApp/Program.cs
@@ -4,76 +4,69 @@ using System.IO;
 using System.Linq;
 
 
-namespace IteratorsApp
+namespace IteratorsApp;
+
+class Program
 {
-    class Program
+    private static void Main()
     {
-        private static void Main()
+        string path = @"..\..\..\Program.cs";
+        if (!File.Exists(path))
+            throw new FileNotFoundException($"Не найден файл по пути \"{path}\"");
+
+        IEnumerable<string> lines1 = ReadFileUsingReturn(path);
+        IEnumerable<string> lines2 = ReadFileUsingLinq(path);
+        IEnumerable<string> lines3 = ReadFileUsingYieldReturn(path);
+
+        PrintFileContents($"Вывод строк из файла {path} через return:", lines1);
+        PrintFileContents($"Вывод строк из файла {path} через LINQ:", lines2);
+        PrintFileContents($"Вывод строк из файла {path} через yield return:", lines3);
+
+        Console.ReadLine();
+    }
+
+    private static IEnumerable<string> ReadFileUsingReturn(string path)
+    {
+        using var streamReader = new StreamReader(path);
+        return ReadLinesFromStream(streamReader);
+    }
+
+    private static IEnumerable<string> ReadFileUsingLinq(string path)
+    {
+        using var streamReader = new StreamReader(path);
+        return ReadLinesFromStream(streamReader).Select(l => l);
+    }
+
+    private static IEnumerable<string> ReadFileUsingYieldReturn(string path)
+    {
+        using var streamReader = new StreamReader(path);
+        foreach (string line in ReadLinesFromStream(streamReader))
+            yield return line;
+    }
+
+    private static IEnumerable<string> ReadLinesFromStream(StreamReader streamReader)
+    {
+        string line;
+        while ((line = streamReader.ReadLine()) != null)
+            yield return line;
+    }
+
+    private static void PrintFileContents(string headerMessage, IEnumerable<string> lines)
+    {
+        try
         {
-            string path = "..\\..\\Program.cs";
-            if (!File.Exists(path))
-                throw new FileNotFoundException($"Не найден файл по пути \"{path}\"");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(headerMessage);
+            Console.ResetColor();
 
-            IEnumerable<string> lines1 = ReadFileUsingYieldReturn(path);
-            IEnumerable<string> lines2 = ReadFileUsingReturn(path);
-            IEnumerable<string> lines3 = ReadFileUsingLinq(path);
-
-            PrintFileContents($"Вывод строк из файла {path} через yield return:", lines1);
-            PrintFileContents($"Вывод строк из файла {path} через return:", lines2);
-            PrintFileContents($"Вывод строк из файла {path} через LINQ:", lines3);
-
-            Console.ReadLine();
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Чтение файла завершилось с ошибкой: {e.Message}");
         }
 
-        private static IEnumerable<string> ReadFileUsingYieldReturn(string path)
-        {
-            using (var streamReader = new StreamReader(path))
-            {
-                foreach (string line in ReadLinesFromStream(streamReader))
-                    yield return line;
-            }
-        }
-
-        private static IEnumerable<string> ReadFileUsingReturn(string path)
-        {
-            using (var streamReader = new StreamReader(path))
-            {
-                return ReadLinesFromStream(streamReader);
-            }
-        }
-
-        private static IEnumerable<string> ReadFileUsingLinq(string path)
-        {
-            using (var streamReader = new StreamReader(path))
-            {
-                return ReadLinesFromStream(streamReader).Select(l => l);
-            }
-        }
-
-        private static IEnumerable<string> ReadLinesFromStream(StreamReader streamReader)
-        {
-            string line;
-            while ((line = streamReader.ReadLine()) != null)
-                yield return line;
-        }
-
-        private static void PrintFileContents(string headerMessage, IEnumerable<string> lines)
-        {
-            try
-            {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine(headerMessage);
-                Console.ForegroundColor = ConsoleColor.White;
-
-                foreach (string line in lines)
-                    Console.WriteLine(line);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Чтение файла завершилось с ошибкой: {e.Message}");
-            }
-
-            Console.WriteLine();
-        }
+        Console.WriteLine();
     }
 }
